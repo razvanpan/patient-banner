cc6f82f5258d491a64a3c1c69e462fb23b65f2b8
Lorena Anghel <anghellorena16@gmail.com>
2020-09-09 14:15:59 +0300

    created react library

---
+++ patient_banner/example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'patient_banner'
+import 'patient_banner/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
